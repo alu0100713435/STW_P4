34d1e352e7e1562eb08244ebb1392e429952bb9b
alu01007134351 <noelpadron1415@gmail.com>
2015-10-26 19:05:58 +0000

    cambios

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,13 @@ var gulp    = require('gulp'),
 var del     = require('del');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
-
-gulp.task('minify', function () {
-  gulp.src('temperature.js')
-  .pipe('/test/index')
-  .pipe(uglify())
-  .pipe(gulp.dest('minified'));
-
-  gulp.src('./index.html')
-    .pipe('/test/index')
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('./minified/'))
-
-  gulp.src('./*.css')
-   .pipe('/test/index')
-   .pipe(minifyCSS({keepBreaks:true}))
-   .pipe(gulp.dest('./minified/'))
-});
-
-gulp.task('clean', function(cb) {
-  del(['minified/*'], cb);
-});
+var mocha   = require('gulp-mocha');
 
 gulp.task('test', function() {
   // Be sure to return the stream
   return gulp.src([])
     .pipe(karma({
-      configFile: 'karma.conf.js',
+      configFile: 'my.conf.js',
       action: 'run'
     }))
     .on('error', function(err) {
@@ -44,7 +24,7 @@ gulp.task('test', function() {
 gulp.task('default', function() {
   gulp.src([])
     .pipe(karma({
-      configFile: 'karma.conf.js',
+      configFile: 'my.conf.js',
       action: 'watch'
     }));
 });
