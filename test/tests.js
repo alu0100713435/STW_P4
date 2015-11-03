3b4f1e3b93e3cf42c592a154132656245ee7d758
alu01007134351 <noelpadron1415@gmail.com>
2015-11-03 16:24:42 +0000

    travis

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-var expect = chai.expect;
+/*var expect = chai.expect;
 
 describe("Test Conversor", function() {
 
@@ -48,5 +48,35 @@ describe("Test Conversor", function() {
       original.value = "3ju";
       calculate();
       expect(converted.innerHTML).to.equal("ERROR! Prueba con algo como esto '-4.2C' ");
+    });
+});*/
+
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0C', function() {
+        original.value = "32F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    });
+    test('45C = 113.0 Farenheit', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+    test('Buenos dias = error', function() {
+        original.value = "Buenos dias";
+        calculate();
+        assert.equal(converted.innerHTML, 'ERROR! Prueba con algo como esto \'-4.2C\' ', /ERROR/);
+    });
+    test('45.3 = error', function() {
+        original.value = "45.3";
+        calculate();
+        assert.notEqual(converted.innerHTML, 113.5, "/ERROR/");
     });
 });
